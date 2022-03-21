b23e0827a5b74319b36252c9c9afcd47a0aa3572
manav aggarwal <manavagr1108@gmail.com>
2022-03-21 14:52:23 +0530

    feat(otp): add otp verification

---
+++ src/api/controllers/auth/ResendOTPVerification.ts
@@ -0,0 +1,25 @@
+import { Handler } from 'express';
+import OTPVerification from '../../models/auth/userOTPVerification';
+import sendOtpVerificationEmail from './SendOTPVerificationEmail';
+
+const ResendOTPVerification:Handler = async ( req , res) => {
+    try {
+        const {username} = req.body;
+        console.log(username)
+        if(!username){
+            console.log("Empty user details are not allowed!")
+            throw Error("Empty user details are not allowed!");
+        } else {
+            await OTPVerification.deleteMany({ username });
+            sendOtpVerificationEmail(username , res );
+        }
+
+    } catch (error) {
+        res.json({
+            status: "FAILED",
+            msg: error
+        })
+    }
+}
+
+export {ResendOTPVerification}
